8a3b58a9ba194b823da113343c54908fdf9109f1
computerProgrammer10 <daniel.huseby26@d155.org>
2024-02-08 11:29:33 -0600

    Initial commit

---
+++ Commission.java
@@ -0,0 +1,16 @@
+public class Commission extends Hourly{
+    private double totalSales, commission;
+    public Commission(String eName, String eAddress, String ePhone,
+                      String socSecNumber, double rate, double commission){
+    super(eName,eAddress,ePhone,socSecNumber, rate);
+    this.commission = commission;
+    }
+
+    public void addSales (double totalSales){this.totalSales += totalSales;}
+    public double pay(){
+        double payment =  super.pay() + (totalSales*commission);
+        totalSales = 0;
+        return payment;
+    }
+    public String toString(){return super.toString() + "\nTotal sales: "+totalSales;}
+}
